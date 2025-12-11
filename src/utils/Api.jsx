fae9e6ea0af76f8b30af8477e12faae2b050a289
VasuKakadiya20 <vasukakadiya1234@gmail.com>
2025-12-11 15:41:58 +0530

    12 commit

--- src/utils/Api.jsx
+++ src/utils/Api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchDataFromApi = async (url) => {
   try {
-    const { data } = await axios.get("http://localhost:4000" + url);
+    const { data } = await axios.get("https://sweets-backend.onrender.com" + url);
     return data;
   } catch (error) {
     console.error(error);
